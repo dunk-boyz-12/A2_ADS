71bf274a4f949d4ccd54dce72850797400dcc08e
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2021-06-14 20:36:22 -0700

    Initial Commit

---
+++ Assignment_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment_2
+//
+//  Created by Nicholas Buras on 6/14/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
